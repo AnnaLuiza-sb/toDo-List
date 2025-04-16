e725767bcc1f4f27f4d44c5c1d6be2430b35a39d
Anna Luiza <luiza.sobral.ls@gmail.com>
2025-04-16 11:03:55 -0300

    Projeto Lista De Tarefas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import styles from './Header.module.css'
 export function Header() {
   return (
     <header className={styles.container}>
-      <img src="/toDo-List/imgs/logo.png" alt="Logo" />
+      <img src="/toDo-List/imgs/logo.svg" alt="Logo" />
     </header>
   )
 }
